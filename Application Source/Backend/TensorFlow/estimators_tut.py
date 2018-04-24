7d3ed219c395a9b26080878d89b87eec8beee8cb
Tobias Bester <u14041368@tuks.co.za>
2018-04-24 17:00:23 +0200

    Started MNIST Tutorial

--- Application Source/Backend/TensorFlow/estimators_tut.py
+++ Application Source/Backend/TensorFlow/estimators_tut.py
@@ -27,5 +27,25 @@
 # A feature column is an object describing how the model should use raw input 
 # data from the features dictionary. The Estimator model would receive this
 # features column. TensorFlow uses the tf.feature_column.
-# For example, a feature column would tell the Estimator to take 4 raw integer
-# values
+#  - For example, a feature column would tell the Estimator to take 4 raw numeric
+# values and represent them as 32-bit floating-point values.
+
+# STEP 3: INSTANTIATE AN ESTIMATOR
+# For classification problems, like the one we have, we use TensorFlow Estimators,
+# such as tf.estimator.DNNClassifier. We could instantiate an estimator with 2
+# hidden layers and 10 nodes in each layer.
+
+# STEP 4: TRAIN, EVALUATE, AND PREDICT
+
+# TRAIN THE MODEL
+# To train the model, we call the classifier's train() function, passing in an
+# input function and a number of steps to train before stopping.
+# The input function contains the data that is set to be trained.
+
+# EVALUATE THE MODEL
+# We use classifier.evaluate() to test how good our model is by passing an
+# function with separate test data. Only one step (or epoch) is completed.
+
+# MAKING PREDICTIONS
+# We use classifer.predict() with an input function of random samples that we
+# thought of to actually predict data.
---
+++ Application Source/Backend/TensorFlow/mnist_tut.py
@@ -0,0 +1,19 @@
+# A guide to TF layers: Building a Convolutional Neural Network:
+# https://www.tensorflow.org/tutorials/layers
+
+# Getting Started
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+# Program logic goes here
+
+
+
+if __name__ == "__main__":
+    tf.app.run()
