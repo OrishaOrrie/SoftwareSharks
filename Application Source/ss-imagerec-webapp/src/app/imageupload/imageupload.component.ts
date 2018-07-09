6845321dfb9adfb938b2e73f04f8dfbe74e6e447
Tobias Bester <u14041368@tuks.co.za>
2018-07-09 11:00:39 +0200

    File Select Uplod now works on server side

--- Application Source/ss-imagerec-webapp/src/app/imageupload/imageupload.component.ts
+++ Application Source/ss-imagerec-webapp/src/app/imageupload/imageupload.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { style } from '@angular/animations';
 /* File Name: imageupload.component
 Version Number: v1.0
@@ -16,6 +17,7 @@ Functional Description: Allows user to upload image and receive classification o
 import { Result } from './result';
 import { IUppy, UppyFile } from 'uppy-store-ngrx';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from '../../../node_modules/rxjs/Observable';
 
 const Uppy = require('uppy/lib/core');
 const Dashboard = require('uppy/lib/plugins/Dashboard');
@@ -36,12 +38,15 @@ export class ImageuploadComponent implements OnInit {
   public results: Result[] = [];
   public instruction: String = 'Click Browse, My Device, or Webcam and Select or Capture an Image to Upload';
   public showSpinner = false;
-  streaming = false;
+  public streaming = false;
   public video = null;
   public canvas = null;
   public image = null;
+  public captureButton = null;
+  public imageToUpload: File = null;
+  public uploadCapture = false;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
     /* this.uppy = Uppy({
@@ -98,7 +103,8 @@ export class ImageuploadComponent implements OnInit {
 
   }
 
-  madeChange() {
+  madeChange(event) {
+    this.imageToUpload = null;
     this.streaming = false;
     const uploadedFile = document.querySelector('input');
     const preview = document.querySelector('.preview');
@@ -110,7 +116,10 @@ export class ImageuploadComponent implements OnInit {
       const newP = document.createElement('p');
       newP.textContent = 'No files currently selected';
       preview.appendChild(newP);
+      console.log('set to null');
+      this.imageToUpload = null;
     } else {
+
       const newP = document.createElement('p');
       const fileSize = this.formattedFileSize(uploadedFile.files[0].size);
       newP.textContent = 'File Name: ' + uploadedFile.files[0].name + ' Size: ' + fileSize;
@@ -119,6 +128,9 @@ export class ImageuploadComponent implements OnInit {
       image.style.setProperty('height', '200px');
       preview.appendChild(newP);
       preview.appendChild(image);
+
+      this.uploadCapture = false;
+      this.imageToUpload = uploadedFile.files[0];
     }
 
   }
@@ -137,12 +149,7 @@ export class ImageuploadComponent implements OnInit {
   }
 
   captureImage() {
-    if (this.hasGetUserMedia()) {
-      console.log('Has getUserMedia');
-    } else {
-      console.log(':(');
-    }
-
+    this.imageToUpload = null;
     const preview = document.querySelector('.preview');
     while (preview.firstChild) {
       preview.removeChild(preview.firstChild);
@@ -152,11 +159,13 @@ export class ImageuploadComponent implements OnInit {
     this.video.textContent = 'Video stream not available';
     this.video.style.setProperty('float', 'left');
 
-    const captureButton = document.createElement('button');
-    captureButton.textContent = 'CAPTURE';
-    captureButton.style.setProperty('padding', '16px 32px');
-    captureButton.style.setProperty('border-radius', '3px');
-    captureButton.style.setProperty('background', 'rgb(255, 255, 255)');
+    this.uploadCapture = true;
+
+    this.captureButton = document.createElement('button');
+    this.captureButton.textContent = 'CAPTURE';
+    this.captureButton.style.setProperty('padding', '16px 32px');
+    this.captureButton.style.setProperty('border-radius', '3px');
+    this.captureButton.style.setProperty('background', 'rgb(255, 255, 255)');
 
     const br = document.createElement('br');
 
@@ -165,13 +174,15 @@ export class ImageuploadComponent implements OnInit {
 
     this.image = document.createElement('img');
 
-    this.startup(this.video, this.canvas, this.image, captureButton);
+    this.startup(this.video, this.canvas, this.image, this.captureButton);
+
+    // this.captureButton.onclick = this.saveCapture;
 
     preview.appendChild(this.video);
     preview.appendChild(this.canvas);
     preview.appendChild(this.image);
     preview.appendChild(br);
-    preview.appendChild(captureButton);
+    preview.appendChild(this.captureButton);
 
   }
 
@@ -185,7 +196,7 @@ export class ImageuploadComponent implements OnInit {
       })
       .catch(function(err) {
         console.log('An error has occured! ' + err);
-    });
+      });
 
     video.addEventListener('canplay', function(ev) {
       if (!this.streaming) {
@@ -198,38 +209,99 @@ export class ImageuploadComponent implements OnInit {
       }
     }, false);
 
-    captureButton.addEventListener('click', function(ev) {
+    captureButton.onclick = function() {
       const context = canvas.getContext('2d');
+      let data = null;
       if (width && height) {
+        this.uploadCapture = true;
         canvas.width = width;
         canvas.height = height;
         context.drawImage(video, 0, 0, width, height);
 
-        const data = canvas.toDataURL('image/png');
-        image.setAttribute('src', data);
+        data = canvas.toDataURL('image/png');
+        image.src = data;
+
       } else {
-        this.clearPhoto(canvas, image);
+        // this.clearPhoto(canvas, image);
       }
-
-      /* TODO: Link Capture Photo to Photo Object that needs to be sent/uploaded */
-
-      ev.preventDefault();
-    }, false);
+    };
 
     this.clearPhoto(canvas, image);
+  }
+
+  /*
+  saveCapture(canvas = this.canvas, video = this.video, image = this.video) {
+    // console.log('Function called');
+
+    const context = canvas.getContext('2d');
+    let data = null;
+    if (video.width && video.height) {
+      this.uploadCapture = true;
+      canvas.width = video.width;
+      canvas.height = video.height;
+      context.drawImage(video, 0, 0, video.width, video.height);
+
+      data = canvas.toDataURL('image/png');
+      image.src = data;
+
+    } else {
+      this.clearPhoto(canvas, image);
+    }
+  } */
+
+  clearPhoto(canvas, image) {
+    const context3 = canvas.getContext('2d');
+    context3.fillStyle = '#FFF';
+    context3.fillRect(0, 0, canvas.width, canvas.height);
+
+    const dataF = canvas.toDataURL('image/png');
+    image.setAttribute('src', dataF);
   }
 
   hasGetUserMedia() {
     return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
   }
 
-  clearPhoto(canvas, image) {
-    const context = canvas.getContext('2d');
-    context.fillStyle = '#FFF';
-    context.fillRect(0, 0, canvas.width, canvas.height);
+  uploadImage() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        /* 'Content-Type': 'multipart/form-data', */
+        'Accept': 'application/json'
+      })
+    };
 
-    const data = canvas.toDataURL('image/png');
-    image.setAttribute('src', data);
+    console.log('Uploading...');
+    if (this.imageToUpload == null) {
+      console.log('No image selected or captured');
+    } else {
+      console.log('Boolean at uploadImage() is: ' + this.uploadCapture);
+      if (this.uploadCapture) {
+        this.getCapturedImage();
+        console.log('Uploading the webcam capture');
+      } else {
+
+        console.log('Uploading the selected file');
+        console.log(this.imageToUpload);
+
+        const dest = 'http://localhost:8000/upload';
+        const formData: FormData = new FormData();
+        formData.append('file', this.imageToUpload, this.imageToUpload.name);
+
+        this.http.post(dest, formData, httpOptions)
+        .subscribe(
+          data => {
+            console.log('SENT!');
+          },
+          error => {
+            console.log(Observable.throw(error));
+          }
+        );
+      }
+    }
+  }
+
+  getCapturedImage() {
+
   }
 
   reloadPage() {
