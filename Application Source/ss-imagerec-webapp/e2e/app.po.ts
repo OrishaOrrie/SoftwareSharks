838c7008b14acbd799ad7cc4afc1b650249fcf68
Tobias Bester <u14041368@tuks.co.za>
2018-07-15 22:40:47 +0200

    Completed E2E tests for all components

--- Application Source/ss-imagerec-webapp/e2e/app.po.ts
+++ Application Source/ss-imagerec-webapp/e2e/app.po.ts
@@ -6,6 +6,90 @@ export class AppPage {
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('app-root .toolbar-title')).getText();
+  }
+
+  getHomeComponent() {
+    return element(by.css('app-home'));
+  }
+
+  getNavbarImageupload() {
+    return element(by.id('navbar-image'));
+  }
+
+  getNavbarUtilities() {
+    return element(by.id('navbar-utilities'));
+  }
+
+  getFooterContactUs() {
+    return element(by.id('footer-contactus'));
+  }
+
+}
+
+export class ImageUploadPage {
+  navigateTo() {
+    return browser.get('/imageupload');
+  }
+
+  getFileInput() {
+    return element(by.id('file-upload'));
+  }
+
+  getUploadButton() {
+    return element(by.className('upload-button'));
+  }
+
+  getResultsList() {
+    return element.all(by.className('results-list'));
+  }
+
+}
+
+export class UtilitiesPage {
+  navigateTo() {
+    return browser.get('/utilities');
+  }
+
+  getResultText() {
+    return element(by.className('result-title')).getText();
+  }
+
+  getSingleWeight() {
+    return element(by.id('single-weight'));
+  }
+
+  getEmptyWeight() {
+    return element(by.id('empty-weight'));
+  }
+
+  getFilledWeight() {
+    return element(by.id('filled-weight'));
+  }
+}
+
+export class ContactUsPage {
+  navigateTo() {
+    return browser.get('/contactus');
+  }
+
+  getNameField() {
+    return element(by.className('name-part'));
+  }
+
+  getEmailField() {
+    return element(by.className('email-part'));
+  }
+
+  getMsgField() {
+    return element(by.id('message'));
+  }
+
+  getSubmitButton() {
+    return element(by.className('sumbit-btn'));
+  }
+
+  getStatusResult() {
+    return element(by.className('status-heading'));
   }
 }
