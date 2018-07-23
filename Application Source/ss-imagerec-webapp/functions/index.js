8e7c624c067bd3a9ebdc1153a4cfd6a097bbda4c
Tobias Bester <u14041368@tuks.co.za>
2018-07-23 11:04:24 +0200

    Added local image-rec dependencies to cloud functions and updated contact us response

--- Application Source/ss-imagerec-webapp/functions/index.js
+++ Application Source/ss-imagerec-webapp/functions/index.js
@@ -1,10 +1,18 @@
 const functions = require('firebase-functions');
 const express= require('express');
+const path = require('path');
 const cors = require('cors');
 const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const _httpCodes = require('./local_modules/HTTPRequests.js');
+const _logger = require('./local_modules/logger.js');
+const pyShell = require('python-shell');
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get('/send', (request, response) => {
   console.log('');
@@ -42,10 +50,11 @@ app.post('/sendmail', (request, response) => {
     text: 'Query from address: ' + request.body.email + '\n' + request.body.text
   }
   console.log(mailOptions);
-  smtpTransport.sendMail(mailOptions, function(error, res) {
+  smtpTransport.sendMail(mailOptions, (error, res) => {
     if(error) {
       console.log(error);
-      response.end('error');
+      response.json({ message: 'Message not sent'});
+      response.status(400).end();
     } else {
       console.log('Message sent: ' + res.message);
       response.json({ message: 'Message sent'});
@@ -55,4 +64,54 @@ app.post('/sendmail', (request, response) => {
 
 });
 
+/**
+  MULTER
+
+let _uploadFilePath = '';
+const storage = multer.diskStorage({
+  destination: function (request, file, cb) {
+    cb(null, 'upload/');
+  },
+  filename: (request, file, cb) => {
+    if (file.mimetype === ('image/jpeg')) {
+      _uploadFilePath = 'upload.jpg';
+      cb(null, _uploadFilePath);
+    } else if (file.mimetype === ('image/png')) {
+      _uploadFilePath = 'upload.png';
+      cb(null, _uploadFilePath);
+    }
+  }
+});
+var upload = multer({ storage: storage });
+*/
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+/**
+  TESTING HTTP REQUESTS
+*/
+app.get('/', (request, response) => {
+  response.status(_httpCodes.RESPONSE.OKAY).send('Hello and Welcome to the Test Environment NodeJS Development Server');
+});
+
+/**
+  IMAGE RECOGNITION SECTION
+
+app.post('/upload', upload.single('file'), (request, response) => {
+  if (!request.file) {
+    console.log('No file received');
+    response.status(_httpCodes.RESPONSE.BAD_REQUEST).send('Invalid File / No File Received');
+  } else {
+    console.log('---------------');
+    console.log('Received File:');
+    console.log('---------------');
+    console.log('Attempting to Uploda Image to Predict Model');
+    pyShell.send('../upload/' + _uploadFilePath);
+    console.log('Image sent to program');
+    pyShell.on('message', (message) => {
+      console.log(message);
+    });
+  }
+});
+*/
 exports.app = functions.https.onRequest(app);
--- Application Source/ss-imagerec-webapp/src/app/contact-us/contact-us.component.ts
+++ Application Source/ss-imagerec-webapp/src/app/contact-us/contact-us.component.ts
@@ -32,6 +32,16 @@ export class ContactUsComponent implements OnInit {
   submitted = false;
 
   /**
+   * Determines whether the spinner is to be displayed or not
+   */
+  showSpinner = false;
+
+  /**
+   * Message received from the server. Displayed on the status card
+   */
+  msgReceived = '';
+
+  /**
    * FormControl used to validate email input
    */
   email = new FormControl('', [Validators.required, Validators.email] );
@@ -71,14 +81,19 @@ export class ContactUsComponent implements OnInit {
     };
 
     console.log('Name: ' + contactName + ' Email: ' + contactEmail + ' Msg: ' + contactMessage);
+    this.showSpinner = true;
 
     this.http.post('https://us-central1-testproject-ee885.cloudfunctions.net/app/sendmail',
     {'subject': contactName, 'text': contactMessage, 'email': contactEmail},
     httpOptions
     ).subscribe(data1 => {
-      console.log(data1);
-      if (data1[0]) {
-        this.submitted = true;
+      this.showSpinner = false;
+      const msgFromServer = data1['message'];
+      this.submitted = true;
+      if (msgFromServer === 'Message sent') {
+        this.msgReceived = 'Thank you for contacting us! We\'ll be in touch ;) ';
+      } else {
+        this.msgReceived = 'Sorry, your message was not sent. Try again later please if you don\'t mind OwO';
       }
     });
 
