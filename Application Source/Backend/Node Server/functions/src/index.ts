d87f31769c4e2592252f9ac5cb5d81b03a38272f
Mark Coetzer <u14044537@tuks.co.za>
2018-08-14 15:37:54 +0200

    Attempted to use prebuilt canvas element

--- Application Source/Backend/Node Server/functions/src/index.ts
+++ Application Source/Backend/Node Server/functions/src/index.ts
@@ -19,7 +19,7 @@ admin.initializeApp({
 // firebase.initializeApp(environment);
 
 export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+  response.send("Hello from Firebase!");
 });
 
 // ====================================================================================================
@@ -39,25 +39,25 @@ const mailTransport = nodemailer.createTransport({
 });
 
 export const sendMail = functions.https.onRequest((request, response) => {
-    console.log('/sendmail received a request\n');
-    console.log('Request: \n');
-    //console.log('Email: ' + request.body.email + '\n');
-    const mailOptions = {
-        from: '"SoftwareSharks"',
-        to: 'mark.coetzerjnr@gmail.com',
-        subject: 'Testing NodeMailer',
-        text: 'Random Text Here'
-    };    
+  console.log('/sendmail received a request\n');
+  console.log('Request: \n');
+  //console.log('Email: ' + request.body.email + '\n');
+  const mailOptions = {
+    from: '"SoftwareSharks"',
+    to: 'mark.coetzerjnr@gmail.com',
+    subject: 'Testing NodeMailer',
+    text: 'Random Text Here'
+  };
 
-    mailTransport.sendMail( mailOptions, (error, info ) => {
-        if (error) {
-            console.log(error);
-            response.status(500).send("Error: Email not sent");
-          } else {
-            console.log("Message sent: " + info.repsonse);
-            response.status(200).send("Email Sent");
-         };
-    });
+  mailTransport.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      response.status(500).send("Error: Email not sent");
+    } else {
+      console.log("Message sent: " + info.repsonse);
+      response.status(200).send("Email Sent");
+    };
+  });
 });
 
 
@@ -70,7 +70,9 @@ export const sendMail = functions.https.onRequest((request, response) => {
 // const storage = firebase.storage();
 // let test = admin.storage().bucket('gs://testproject-ee885.appspot.com').get()
 
-// import * as tf from '@tensorflow/tfjs';
+import * as tf from '@tensorflow/tfjs';
+const {Image} = require('canvas-prebuilt');
+//import * as canvas from 'canvas';
 
 export const upload = functions.https.onRequest(async (request, response) => {
   // response.send("Hello from Firebase!");
@@ -79,18 +81,50 @@ export const upload = functions.https.onRequest(async (request, response) => {
   // console.log(admin.storage().bucket().file('images/vicious_dog_0.png'));
   //console.log(admin.storage().bucket().getFiles('images/vicious_dog_0.png'));
   try {
-    const snapshot = await admin.storage().bucket().file('images/vicious_dog_0.png').getMetadata();
-    const metadata = snapshot[0];
-    console.log(`File: ${metadata.name}`);
-    console.log(`Bucket: ${metadata.bucket}`);
-    console.log(`Size: ${metadata.size}`);
+    // const snapshot = await admin.storage().bucket().file('images/vicious_dog_0.png').getMetadata();
+    // const metadata = snapshot[0];
+    // console.log(`File: ${metadata.name}`);
+    // console.log(`Bucket: ${metadata.bucket}`);
+    // console.log(`Size: ${metadata.size}`);
+    // const snapshot = 
+    await admin.storage().bucket().file('tfjs/model.json').get()
+      .then(imageSnapshot => {
+        loadModel(imageSnapshot).then((model)=>{
+          const image = new Image();
+          image.src = 'https://i.pinimg.com/236x/f8/ca/43/f8ca4391f64eb8bba6fd908f4046fbca--funny-chihuahua-quotes-funny-chihuahuas.jpg';
+          const tensor = tf.fromPixels(image);
+          console.log(model.predict(tensor));
+        }).catch(error => {console.log(error)});
+        // return (async () => {
+        //   await tf.tidy(() => {
+        //     const predictions = model.predict(img);
+        //     // let img = tf.fromPixels();
+        //     // img = img.reshape([3, 229, 229]);
+        //     // img = tf.cast(img, 'float32');
+        //     // const output = this.model.predict(img) as any;
+        //     // this.predictions = Array.from(output.dataSync());
+        //     // console.log(this.predictions);
+        //   });
+        // });
+      }).catch(error => {
+        console.log(error);
+      });
     response.status(200).send('Success');
   } catch (error) {
     console.log(error);
     response.status(500).send(error);
   }
- });
+});
 
+async function loadModel(onlineModel) {
+  try {
+    const model = await tf.loadModel(onlineModel);
+    return model;
+  } catch (error) {
+    return null;
+    //return "ERROR: Model Failed Loaded: " + error;
+  }
+}
 //  async function loadModel() {
 //   const ref = storage.ref('tfjs/model.json');
 //   try {
