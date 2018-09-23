008ac71c868c6e1d3d11c51d78e0da48b818677e
Mark Coetzer <u14044537@tuks.co.za>
2018-09-23 21:09:28 +0200

    Created UI/Layout Component

--- Application Source/ss-dashboard/src/app/ui/ui.module.ts
+++ Application Source/ss-dashboard/src/app/ui/ui.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [LayoutComponent]
 })
 export class UiModule { }
