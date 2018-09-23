170b7bd3008f99cf99a1c17421125b004e8aa278
Mark Coetzer <u14044537@tuks.co.za>
2018-09-23 21:09:43 +0200

    Created UI/Header component

--- Application Source/ss-dashboard/src/app/ui/ui.module.ts
+++ Application Source/ss-dashboard/src/app/ui/ui.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [LayoutComponent]
+  declarations: [LayoutComponent, HeaderComponent]
 })
 export class UiModule { }
