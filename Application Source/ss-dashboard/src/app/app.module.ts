70663c1d0ad4ab09be283a7bf817f999b26127f2
Mark Coetzer <u14044537@tuks.co.za>
2018-09-23 21:08:02 +0200

    Scaffholded Dashboard component

--- Application Source/ss-dashboard/src/app/app.module.ts
+++ Application Source/ss-dashboard/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { DashComponent } from './dash/dash.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    DashComponent
   ],
   imports: [
     BrowserModule
