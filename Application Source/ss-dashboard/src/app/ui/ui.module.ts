ed7a34e035246f2b64c30fa94b7cf0b11d944339
Mark Coetzer <u14044537@tuks.co.za>
2018-09-23 21:10:01 +0200

    Generated UI/Footer component

--- Application Source/ss-dashboard/src/app/ui/ui.module.ts
+++ Application Source/ss-dashboard/src/app/ui/ui.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [LayoutComponent, HeaderComponent]
+  declarations: [LayoutComponent, HeaderComponent, FooterComponent]
 })
 export class UiModule { }
