1d391a8b0c08c091dc44751aee5eedbf8c6e4119
Mark Coetzer <u14044537@tuks.co.za>
2018-09-24 19:33:37 +0200

    Setup AngularFire skeleton

--- Application Source/ss-dashboard/src/app/app.module.ts
+++ Application Source/ss-dashboard/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { ParticlesModule } from 'angular-particle';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { DashComponent } from './dash/dash.component';
 import { UiModule } from './ui/ui.module';
+import { ParticlesModule } from 'angular-particle';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from './environment/environment';
 
 @NgModule({
   declarations: [
@@ -17,7 +21,11 @@ import { UiModule } from './ui/ui.module';
   imports: [
     BrowserModule,
     UiModule,
-    ParticlesModule
+    ParticlesModule,
+    AngularFireModule.initializeApp(environment.firebase), // imports firebase/app needed for everything
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule // imports firebase/storage only needed for storage features
   ],
   providers: [],
   bootstrap: [AppComponent]
