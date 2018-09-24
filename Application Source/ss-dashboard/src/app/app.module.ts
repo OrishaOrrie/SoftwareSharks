449e630b08925549d449749e2a2a6d785ca8befd
Mark Coetzer <u14044537@tuks.co.za>
2018-09-24 19:53:40 +0200

    Added routing and User Profile

--- Application Source/ss-dashboard/src/app/app.module.ts
+++ Application Source/ss-dashboard/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CoreModule } from './core/core.module';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -12,19 +13,37 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from './environment/environment';
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
+import { UserProfileComponent } from './views/user-profile/user-profile.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '',
+    redirectTo: '/heroes',
+    pathMatch: 'full'
+  },
+  { path: 'profile', component: UserProfileComponent,  canActivate: [AuthGuard] },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    DashComponent
+    DashComponent,
+    PageNotFoundComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
     UiModule,
     ParticlesModule,
     AngularFireModule.initializeApp(environment.firebase), // imports firebase/app needed for everything
-    CoreModule
+    CoreModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Application Source/ss-dashboard/src/app/core/auth.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+
+import { AuthService} from './auth.service';
+import { Observable } from 'rxjs';
+import { tap, map, take } from 'rxjs/operators';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private auth: AuthService, private router: Router) {}
+
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> {
+
+      return this.auth.user.pipe(
+           take(1),
+           map(user => !!user),
+           tap(loggedIn => {
+             if (!loggedIn) {
+               console.log('access denied');
+               this.router.navigate(['/login']);
+             }
+         })
+    );
+  }
+}
