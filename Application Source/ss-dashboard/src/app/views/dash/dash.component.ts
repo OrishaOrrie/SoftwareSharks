90b03e320f658cd9f697e6ac918c7b54c4152a32
Mark Coetzer <u14044537@tuks.co.za>
2018-09-25 13:16:06 +0200

    Passed AuthService to dashboard

--- Application Source/ss-dashboard/src/app/views/dash/dash.component.ts
+++ Application Source/ss-dashboard/src/app/views/dash/dash.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../core/auth.service';
 
 @Component({
   selector: 'app-dash',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AuthService) { }
 
   ngOnInit() {
   }
