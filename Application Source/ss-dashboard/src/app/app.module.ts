1edfb612dd4e1fe790c4d676aa7f622b100acbc6
Mark Coetzer <u14044537@tuks.co.za>
2018-09-25 13:52:53 +0200

    Made particile system template inline

--- Application Source/ss-dashboard/src/app/app.module.ts
+++ Application Source/ss-dashboard/src/app/app.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 import { CoreModule } from './core/core.module';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ParticlesModule } from 'angular-particle';
+import { AngularFireModule } from '@angular/fire';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
 import { DashComponent } from './views/dash/dash.component';
 import { UiModule } from './ui/ui.module';
-import { ParticlesModule } from 'angular-particle';
-import { AngularFireModule } from '@angular/fire';
 import { environment } from './environment/environment';
 import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
 import { UserProfileComponent } from './views/user-profile/user-profile.component';
