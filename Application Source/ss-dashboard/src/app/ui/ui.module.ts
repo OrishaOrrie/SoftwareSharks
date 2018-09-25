dec02ac4fb79933ed6a81e20004c8fb90e4c1ba5
Mark Coetzer <u14044537@tuks.co.za>
2018-09-25 13:00:34 +0200

    generated Side Nav component skeleton

--- Application Source/ss-dashboard/src/app/ui/ui.module.ts
+++ Application Source/ss-dashboard/src/app/ui/ui.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [LayoutComponent, HeaderComponent, FooterComponent],
+  declarations: [LayoutComponent, HeaderComponent, FooterComponent, SideNavComponent],
   exports: [
     LayoutComponent
   ]
