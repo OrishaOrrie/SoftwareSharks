48a7ea2fefde1d9b3e341c9f5570dcd3f8b5e16d
Mark Coetzer <u14044537@tuks.co.za>
2018-09-26 09:53:13 +0200

    Added some profile layout

--- Application Source/ss-dashboard/src/app/app-routing.module.ts
+++ Application Source/ss-dashboard/src/app/app-routing.module.ts
@@ -39,7 +39,7 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes, { enableTracing: true })],
+  imports: [RouterModule.forRoot(appRoutes, { enableTracing: false })],
   exports: [
     RouterModule
   ],
--- Application Source/ss-dashboard/src/app/views/user-profile/user-profile.component.ts
+++ Application Source/ss-dashboard/src/app/views/user-profile/user-profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../core/auth.service';
+import { Observable } from 'rxjs';
+import { share } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,10 +9,12 @@ import { AuthService } from '../../core/auth.service';
   styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent implements OnInit {
+  public user: Observable<{}>;
 
   constructor(public auth: AuthService) { }
 
   ngOnInit() {
+    this.user = this.auth.user.pipe(share());
   }
 
 }
