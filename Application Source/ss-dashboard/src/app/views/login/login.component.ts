bcc44d1acbc8a344aa1e8a9e498b22a968551fbd
Mark Coetzer <u14044537@tuks.co.za>
2018-09-26 21:43:22 +0200

    Fixed typo

--- Application Source/ss-dashboard/src/app/views/login/login.component.ts
+++ Application Source/ss-dashboard/src/app/views/login/login.component.ts
@@ -14,7 +14,7 @@ export class LoginComponent implements OnInit {
 
   constructor(  private route: ActivatedRoute,
                 private router: Router,
-                private alertService: AlertService,
+                public alertService: AlertService,
                 public auth: AuthService) { }
 
   ngOnInit() {
