1559523c9ec5a16f8e9b5f1c1581496c6543477b
Mark Coetzer <u14044537@tuks.co.za>
2018-09-26 10:47:42 +0200

    Added code doc for model class
    Co-Authored-By: tobiasbester <tobiasbester@users.noreply.github.com>

--- Application Source/ss-dashboard/src/app/shared/models/model.ts
+++ Application Source/ss-dashboard/src/app/shared/models/model.ts
@@ -1,20 +1,50 @@
 export class Model {
+    /**
+     * Represents the name of the model.
+     */
     private _name: string;
+
+    /**
+     * Get accessor for _name member variable.
+     * @returns The value of the _name member variable as a string.
+     */
     public get name(): string {
         return this._name;
     }
+
+    /**
+     * Set modifier for the _name member variable.
+     * @param value  The updated value (of type string) to set _name as.
+     */
     public set name(value: string) {
         this._name = value;
     }
 
+    /**
+     * Represents the list of categories contained within the model.
+     */
     private _categories: string[];
+
+    /**
+     * Get accessor for _categories member variable.
+     * @returns The value of the _categories member variable as a string[].
+     */
     public get categories(): string[] {
         return this._categories;
     }
+
+    /**
+     * Set modifier for the _categories member variable.
+     * @param value  The updated value (of type string[]) to set _categories as.
+     */
     public set categories(value: string[]) {
         this._categories = value;
     }
 
+    /**
+     * Adds a Category or list of Categories to _categories list.
+     * @param category  The category or list of categories to add. Can be either type: string | string[]
+     */
     public addCategory(category: string | string[]) : void {
         if (typeof category == "string") {
             this._categories.push(category);
@@ -24,6 +54,10 @@ export class Model {
         }
     }
 
+    /**
+     * Removes a Category from _categories list.
+     * @param category  The category to remove.
+     */
     public removeCategory(category: string) {
         const index = this._categories.indexOf(category);
         this._categories.splice(index, 1);
