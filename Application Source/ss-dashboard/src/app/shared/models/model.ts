9347b3d83b74482a509723c6abd341e22cd49d48
Mark Coetzer <u14044537@tuks.co.za>
2018-09-26 10:35:57 +0200

    Further defined model class
    Co-Authored-By: tobiasbester <tobiasbester@users.noreply.github.com>

--- Application Source/ss-dashboard/src/app/shared/models/model.ts
+++ Application Source/ss-dashboard/src/app/shared/models/model.ts
@@ -1,3 +1,31 @@
 export class Model {
-    
+    private _name: string;
+    public get name(): string {
+        return this._name;
+    }
+    public set name(value: string) {
+        this._name = value;
+    }
+
+    private _categories: string[];
+    public get categories(): string[] {
+        return this._categories;
+    }
+    public set categories(value: string[]) {
+        this._categories = value;
+    }
+
+    public addCategory(category: string | string[]) : void {
+        if (typeof category == "string") {
+            this._categories.push(category);
+        }
+        else {
+            this._categories = this._categories.concat(category);
+        }
+    }
+
+    public removeCategory(category: string) {
+        const index = this._categories.indexOf(category);
+        this._categories.splice(index, 1);
+    }
 }
