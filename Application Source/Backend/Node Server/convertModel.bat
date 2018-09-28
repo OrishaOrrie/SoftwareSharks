2d9d2bb3018b7aea84951deea9bc96a4c9be4121
yoni lew <u13318765@tuks.co.za>
2018-09-28 10:18:12 +0200

    Purged old Files, and set up new file structure

--- Application Source/Backend/Node Server/convertModel.bat
+++
@@ -1,6 +0,0 @@
-set /p ModelInputPath=Please enter the location of model file: 
-set /p ModelOutputPath=Please enter the location to store model file: 
-
-tensorflowjs_converter --input_format keras --output_format tensorflowjs "%ModelInputPath%" "%ModelOutputPath%\tfjs"
-move "%ModelOutputPath%\tfjs\model.json" "%ModelOutputPath%"
-pause
