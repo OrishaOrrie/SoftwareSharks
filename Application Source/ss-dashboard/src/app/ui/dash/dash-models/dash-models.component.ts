a83448e8dcd389ca1d1c777ca84b389f2eabb7c1
Mark Coetzer <u14044537@tuks.co.za>
2018-09-29 14:33:14 +0200

    Got basic model display working

--- Application Source/ss-dashboard/src/app/ui/dash/dash-models/dash-models.component.ts
+++ Application Source/ss-dashboard/src/app/ui/dash/dash-models/dash-models.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ModelsService } from '../../../core/data/models.service';
+import { switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { async } from 'rxjs/internal/scheduler/async';
 
 @Component({
   selector: 'app-dash-models',
@@ -8,15 +11,30 @@ import { ModelsService } from '../../../core/data/models.service';
 })
 export class DashModelsComponent implements OnInit {
 
-  constructor(public modelsService: ModelsService) { }
+  constructor(public modelsService: ModelsService) { 
+  }
 
   ngOnInit() {
-    this.getModels();
+    // this.getModels();
   }
 
   public getModels() {
-    this.modelsService.getModelsAsPromise().then((model) => {
-      console.log(model.name);
-    });
+    console.log("Calling getModels");
+    this.modelsService.modelsObservable.pipe(
+      switchMap(models => {
+        if (models) {
+          // return this.afs.doc<User>(`users/${user.uid}`).valueChanges();
+          console.log("Kind of not broken");
+          console.log(models);
+          return of(null);
+        } else {
+          console.log("Dis is broken af");
+          return of(null);
+        }
+      })
+    );
+    // this.modelsService.getModelsAsPromise().then((model) => {
+    //   console.log(model.name);
+    // });
   }
 }
