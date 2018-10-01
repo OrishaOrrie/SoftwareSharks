74602f2ac2e5a51757208f7999696f80221e1b71
Mark Coetzer <u14044537@tuks.co.za>
2018-10-02 01:41:33 +0200

    Wrote Basic Unit Tests for AlertsService

--- Application Source/ss-dashboard/src/app/core/alert/alertservice.spec.ts
+++ Application Source/ss-dashboard/src/app/core/alert/alertservice.spec.ts
@@ -1,14 +1,37 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AlertService } from './alert.service';
+import { Alert } from '../../shared/models/alert';
+import { AlertType } from '../../shared/models/AlertType';
 
 describe('AlertService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: AlertService;
+  beforeEach(() => {
+    TestBed.configureTestingModule({ providers: [AlertService] });
+    service = TestBed.get(AlertService);
+  });
 
   it('should be created', () => {
-    const service: AlertService = TestBed.get(AlertService);
     expect(service).toBeTruthy();
   });
 
-  
+  it('should clear alerts array', () => {
+    service.clear();
+    expect(service.alerts.length).toEqual(0);
+  });
+
+  it('should append alerts array', () => {
+    const currentLength = service.alerts.length;
+    service.add(new Alert(AlertType.Info, 'Test Alert'));
+    expect(service.alerts.length).toBeGreaterThan(currentLength);
+  });
+
+  it('should remove alert from alerts array', () => {
+    const alert: Alert = new Alert(AlertType.Info, 'Test Alert');
+    service.add(alert);
+    const currentLength = service.alerts.length;
+    service.removeAlert(alert);
+    expect(service.alerts.length).toBeLessThan(currentLength);
+  });
+
 });
