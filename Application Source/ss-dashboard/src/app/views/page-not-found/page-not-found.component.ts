4327b5c3a035ef3b1d2b97ee6acd07bdf0a1a708
Mark Coetzer <u14044537@tuks.co.za>
2018-10-01 21:14:37 +0200

    Reformatted Hero Text on Landing page

--- Application Source/ss-dashboard/src/app/views/page-not-found/page-not-found.component.ts
+++ Application Source/ss-dashboard/src/app/views/page-not-found/page-not-found.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Location} from '@angular/common';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-page-not-found',
