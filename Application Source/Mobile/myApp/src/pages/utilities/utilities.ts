7e785aa24a7c8ea1f84b6799a03541306c6cd383
TobiasBester <36543256+TobiasBester@users.noreply.github.com>
2018-10-04 15:04:07 +0200

    Added comments to Utilities component

--- Application Source/Mobile/myApp/src/pages/utilities/utilities.ts
+++ Application Source/Mobile/myApp/src/pages/utilities/utilities.ts
@@ -3,6 +3,23 @@ import { NavController, NavParams, AlertController} from 'ionic-angular';
 import { ModalController } from 'ionic-angular';
 // import { ContactPage } from '../contact/contact';
 import { AboutPage } from '../about/about';
+
+/**
+ * File Name:       utilities.ts
+ * Version Number:  v1.0
+ * Author Name:     Orisha Orrie
+ * Project Name:    Ninshiki
+ * Organization:    Software Sharks
+ * Manual:  Refer to the Ninshiki User Manual at https://github.com/OrishaOrrie/SoftwareSharks/blob/master/Documentation/User%20Manual.pdf
+ * Update History:
+ * ------------------------------------------
+ * Date         Author        Description
+ * 21/07/2018   Orisha        Created component
+ * ------------------------------------------
+ * Functional Description:
+ *  Provides ability for user to conduct weight analysis
+ */
+
 /**
  * Generated class for the UtilitiesPage page.
  *
@@ -14,25 +31,23 @@ import { AboutPage } from '../about/about';
   selector: 'page-utilities',
   templateUrl: 'utilities.html',
 })
-
 export class UtilitiesPage {
-	Math: Math = Math;
-	totalObjects : number =null;
+  
+  /**
+   * An instance of the JavaScript Math class required to use the floor function
+   */
+  Math: Math = Math;
+  
   single_item = 1.0;
   empty_bucket = 2.0;
   filled_bucket = 10.0;
   
+  /**
+   * Determines what is the resulting weight value to be displayed. Handles invalid input values by returning
+   * appropriate validation messages
+   * @returns A string value that is displayed in the result card on the page
+   */
   hello = (() => {
-    // while(emp>=fil && sing>=fil)
-    //   {
-    //     return 0;
-    //   }
-    //   if(!sing || !emp || !fil)
-    //   {
-    //     return 0;
-    //   }
-    //   this.totalObjects = ((fil - emp)/sing);
-    //   return this.totalObjects;
 
     if (!this.empty_bucket || !this.filled_bucket || !this.single_item) {
         return 'Weight inputs cannot be empty';
@@ -51,44 +66,30 @@ export class UtilitiesPage {
     }
 
     return 'Number of items: ' + Math.floor((this.filled_bucket - this.empty_bucket) / this.single_item);
-
   });
 
+  /**
+   * @ignore
+   */
   constructor(public navCtrl: NavController, public navParams: NavParams, 
     public alertController: AlertController, public modalCtrl : ModalController) {
-      
 
   }
 
-  openModal()
-  {
+  /**
+   * Opens the About page modal
+   */
+  openModal() {
     var data = { message : 'hello world' };
     var homePage = this.modalCtrl.create(AboutPage,data);
     homePage.present();
   }
+
+  /**
+   * @ignore
+   */
   ionViewDidLoad() {
     console.log('ionViewDidLoad UtilitiesPage');
-  }
-
-  // errorAlert =function() {
-  //   let erralert = this.alertController.create({
-  //     title: 'Message sent!',
-  //     subTitle: 'Your message has been sent. A member of our team will get back to you as soon as possible.',
-  //     buttons: ['Dismiss']
-  //   })
-  // }
-
-    presentAlert =function() {
-      let alert = this.alertController.create({
-        title: 'Total amount of items',
-        subTitle: this.totalObjects,
-        buttons: ['Dismiss']
-      });
-      alert.present();
-    }
-
-    //single filed empty
-  
-   
+  }  
 
 }
