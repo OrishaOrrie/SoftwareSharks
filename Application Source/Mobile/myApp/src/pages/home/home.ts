5f86525766e3a91069dd9d66b9edd1891e7281a9
TobiasBester <36543256+TobiasBester@users.noreply.github.com>
2018-10-04 14:56:20 +0200

    Added comments to Results component

--- Application Source/Mobile/myApp/src/pages/home/home.ts
+++ Application Source/Mobile/myApp/src/pages/home/home.ts
@@ -4,17 +4,39 @@ import { ModalController } from 'ionic-angular';
 // import { ContactPage } from '../contact/contact';
 import { AboutPage } from '../about/about';
 //import {ContactPage} from '../contact/contact';
+
+/**
+ * File Name:       home.ts
+ * Version Number:  v1.0
+ * Author Name:     Orisha Orrie
+ * Project Name:    Ninshiki
+ * Organization:    Software Sharks
+ * Manual:  Refer to the Ninshiki User Manual at https://github.com/OrishaOrrie/SoftwareSharks/blob/master/Documentation/User%20Manual.pdf
+ * Update History:
+ * ------------------------------------------
+ * Date         Author        Description
+ * 21/07/2018   Orisha        Created component
+ * ------------------------------------------
+ * Functional Description:
+ *  Serves as a facade for the application
+ */
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
+  /**
+   * @ignore
+   */
   constructor(public navCtrl: NavController,public modalCtrl : ModalController) {
-  		//contactPage = ContactPage;
   }
-  openModal()
-  {
+
+  /**
+   * Opens the About modal page
+   */
+  openModal() {
     var data = { message : 'hello world' };
     var homePage = this.modalCtrl.create(AboutPage,data);
     homePage.present();
--- Application Source/Mobile/myApp/src/pages/results/results.ts
+++ Application Source/Mobile/myApp/src/pages/results/results.ts
@@ -9,27 +9,63 @@ import { ModelLoaderProvider } from './../../providers/model-loader/model-loader
  * Ionic pages and navigation.
  */
 
+ /**
+ * File Name:       results.ts
+ * Version Number:  v1.0
+ * Author Name:     Orisha Orrie
+ * Project Name:    Ninshiki
+ * Organization:    Software Sharks
+ * Manual:  Refer to the Ninshiki User Manual at https://github.com/OrishaOrrie/SoftwareSharks/blob/master/Documentation/User%20Manual.pdf
+ * Update History:
+ * ------------------------------------------
+ * Date         Author        Description
+ * 1/08/2018   Orisha        Created component
+ * ------------------------------------------
+ * Functional Description:
+ *  Displays results as predicted by the TensorFlowJS model
+ */
+
 @Component({
   selector: 'page-results',
   templateUrl: 'results.html',
 })
 export class ResultsPage {
 
+  /**
+   * Array of result objects, which contain the category name, likeliness, and item web links, if specified.
+   * This array is used to display the list of results in the page
+   */
   public resultPreds = [];
+
+  /**
+   * Fetches the resultPreds from the model loader provider
+   * @param navCtrl Controls navigation
+   * @param navParams Controls parameters passed in during navigation
+   * @param viewCtrl Controls the current view
+   * @param modelLoader The ModelLoader provider that handles all image classification requests
+   */
   constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController, public modelLoader : ModelLoaderProvider) {
-      console.log('UserId', navParams.get('resultPreds'));
       this.resultPreds= modelLoader.getResults();
-      // this.resultPreds = this.formatClassNames(this.resultPreds);
   }
 
+  /**
+   * @ignore
+   */
   ionViewDidLoad() {
     console.log('ionViewDidLoad ResultsPage');
   }
 
+  /**
+   * Closes the modal when the back button is pressed
+   */
   public closeModal(){
     this.viewCtrl.dismiss();
   }
 
+  /**
+   * Opens a link to the Bramhope store for the corresponding item that was pressed
+   * @param url Passed in from the item that is clicked. The corresponding URL is stored in the classes JSON file
+   */
   openBrowserToBramhope(url) {
     if (url == '') {
       return false;
