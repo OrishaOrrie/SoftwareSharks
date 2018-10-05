3c04362a0d6835e215ae6335173329a244ad2b76
TobiasBester <36543256+TobiasBester@users.noreply.github.com>
2018-10-05 08:46:58 +0200

    Made trainModule block until image scraper is done

--- Application Source/Backend/Image Scraper/index.js
+++ Application Source/Backend/Image Scraper/index.js
@@ -10,109 +10,128 @@ if (process.argv[3]) {
   headlessMode = process.argv[3];
 }
 var ImageScraper = function ImageScraper(searchTerm) {
-(async () => {
-  const browser = await puppeteer.launch({
-    headless: headlessMode
-  });
-
-  //const searchTerm = process.argv[2];
-  const dirTerm = searchTerm.replace(/ /g, '_');
-
-  // Go to Google Images
-  console.log('Don\'t mind me just popping in to Google Images');
-  const page = await browser.newPage();
-  await page.goto("https://images.google.com");
-
-  // Input the search term and hit Enter
-  console.log('Hello can I get uhhhh some ' + searchTerm);
-  await page.type('#lst-ib', searchTerm);
-  await page.keyboard.press('Enter');
-  await page.waitForSelector('#hdtb-msb-vis');
-
-  await page.addScriptTag({url: 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js'});
-
-  // Scroll to the bottom of the page
-  console.log('\nNow I am scrolling to the bottom of the page... this may take a while');
-  await page.evaluate(async() => {
-    await new Promise((resolve, reject) => {
-      let totalHeight = 0;
-      let distance = 100;
-      let timer = setInterval(() => {
-        let scrollHeight = document.body.scrollHeight;
-        window.scrollBy(0, distance);
-        totalHeight += distance;
-        if (totalHeight >= scrollHeight) {
-          clearInterval(timer);
-          resolve();
-        }
-      }, 100);
-    });
-  }).then(() => {
-    console.log('Reached end of page');
-  });
-
-  let numImages = 0;
-  numImages = await page.evaluate(() => {
-    return document.querySelectorAll('.rg_di').length;
-  });
-
-  console.log(numImages + ' images found');
-
-  // Get the image source urls of all the images found and add them to an array
-  let urls = [];
-  for (let i = 0; i < numImages; i++) {
-    urls.push( await page.evaluate((i) => {
-      let index = '[data-ri=\"' + i + '\"]';
-      let metaText = document.querySelector(index).childNodes[2].innerHTML;
-      return JSON.parse(metaText).ou;
-    }, i));
-  }
-
-  // get all the images at the urls
-  console.log('getting image download URLs');
-  let numDown = 0;
-
-  for (let i = 0; i < urls.length; i++) {
-    // console.log(urls[i]);
-    let dir = 'downloaded_images/' + dirTerm + '/';
-    if (!fs.existsSync(dir)){
-      fs.mkdirSync(dir);
-    }
-
-    const fileName = dir + i + '.jpg';
-    let file = fs.createWriteStream(fileName);
-    let protocol = http;
-    if (urls[i].slice(0,5) == 'https') {
-      protocol = https;
-    } else {
-      protocol = http;
-    }
-    let request = protocol.get(urls[i], function(response) {
-      response.pipe(file);
-      file.on('finish', function(cb) {
-        file.close(cb);
-        console.log(i + ': ' + urls[i]);
-        numDown++;
-        console.log(numDown + ' images downloaded');
-        // if (cb) cb(err.message);
+  return new Promise((resolve, reject) => {
+    (async () => {
+      const browser = await puppeteer.launch({
+        headless: headlessMode
       });
-    }).on('error', function(err, cb) {
-      fs.unlinkSync(fileName);
-      numDown++;
-      console.log(i + ' was deleted');
-      console.log(numDown + ' images completed');
-      // if (cb) cb(err.message);
-    });
+  
+      //const searchTerm = process.argv[2];
+      const dirTerm = searchTerm.replace(/ /g, '_');
+  
+      // Go to Google Images
+      console.log('Don\'t mind me just popping in to Google Images');
+      const page = await browser.newPage();
+      await page.goto("https://images.google.com");
+  
+      // Input the search term and hit Enter
+      console.log('Hello can I get uhhhh some ' + searchTerm);
+      await page.type('#lst-ib', searchTerm);
+      await page.keyboard.press('Enter');
+      await page.waitForSelector('#hdtb-msb-vis');
+  
+      await page.addScriptTag({url: 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js'});
+  
+      // Scroll to the bottom of the page
+      console.log('Now I am scrolling to the bottom of the page... this may take a while');
+      await page.evaluate(async() => {
+        await new Promise((resolve, reject) => {
+          let totalHeight = 0;
+          let distance = 100;
+          let timer = setInterval(() => {
+            let scrollHeight = document.body.scrollHeight;
+            window.scrollBy(0, distance);
+            totalHeight += distance;
+            if (totalHeight >= scrollHeight) {
+              clearInterval(timer);
+              resolve();
+            }
+          }, 100);
+        });
+      }).then(() => {
+        console.log('Reached end of page');
+      });
+  
+      let numImages = 0;
+      numImages = await page.evaluate(() => {
+        return document.querySelectorAll('.rg_di').length;
+      });
+  
+      console.log(numImages + ' images found');
+  
+      // Get the image source urls of all the images found and add them to an array
+      let urls = [];
+      for (let i = 0; i < numImages; i++) {
+        urls.push( await page.evaluate((i) => {
+          let index = '[data-ri=\"' + i + '\"]';
+          let metaText = document.querySelector(index).childNodes[2].innerHTML;
+          return JSON.parse(metaText).ou;
+        }, i));
+      }
+  
+      // get all the images at the urls
+      console.log('getting image download URLs');
+      let numDown = 0;
+  
+      let looper = new Promise((resolve) => {
+        setTimeout(() => {
+          for (let i = 0; i < urls.length; i++) {
+            // console.log(urls[i]);
+            let j = 0;
+  
+            let dir = 'downloaded_images/' + dirTerm + '/';
+            if (!fs.existsSync(dir)){
+              fs.mkdirSync(dir);
+            }
+      
+            const fileName = dir + i + '.jpg';
+            let file = fs.createWriteStream(fileName);
+            let protocol = http;
+            if (urls[i].slice(0,5) == 'https') {
+              protocol = https;
+            } else {
+              protocol = http;
+            }
+  
+            let request = protocol.get(urls[i], function(response) {
+              response.pipe(file);
+              file.on('finish', function(cb) {
+                file.close(cb);
+                console.log(i + ': ' + urls[i]);
+                numDown++;
+                console.log(numDown + ' images downloaded');
 
-    // If the download doesn't complete within a minute, then cancel the download
-    // Number is milliseconds, so 120 000 is 120 seconds, or 2 minutes
-    request.setTimeout(120000, ()=> {
-      request.abort();
-    })
-  };
-  console.log("Finished Scraping for "+dirTerm);
-  await browser.close();
-})();
+                /**
+                 * If the number of images processed is within 50 of the total images identified, then resolve this promise
+                 */
+                if (numDown > numImages - 50) {
+                  resolve('\nYEEEEEEEET\n');
+                  clearTimeout(); // doesn't work
+                  return; // doesn't work
+                }
+                if (cb) cb(err.message);
+              });
+            }).on('error', function(err, cb) {
+              fs.unlinkSync(fileName);
+              numDown++;
+              console.log(i + ' was deleted');
+              console.log(numDown + ' images completed');
+              // if (cb) cb(err.message);
+            });
+    
+          };
+        }, 30000);
+      });
+  
+      await browser.close();
+
+      let result = await looper;
+      console.log(result);
+      resolve();
+      console.log("Finished Scraping for "+dirTerm);
+    })();
+  });  
+  
 }
 //ImageScraper("Angry");
 module.exports.ImageScraper = ImageScraper;
--- Application Source/Backend/Node Server/src/modules/trainModule.js
+++ Application Source/Backend/Node Server/src/modules/trainModule.js
@@ -11,22 +11,56 @@
 */
 var ImageScraper = require("../../../Image Scraper/index")
 
-var trainModule = function trainModule(categories) {
+var trainModule = async function trainModule(categories) {
     console.log("Training Module");
     //console.log(categories);
     
     const header=categories["header"];
     categories=categories["categories"];
     
-    //Scrape Images
-    for(var category in categories) {
-        console.log("Image scraping for: "+categories[category]);
-        ImageScraper.ImageScraper(categories[category]);
-    }
+    //Scrape Images all categories simultaneously
+    // let numCompleted = 0;
+    // for(var category in categories) {
+    //     console.log("Image scraping for: "+categories[category]);
+    //     ImageScraper.ImageScraper(categories[category]).then(() => {
+    //         console.log(++numCompleted + ' categories completed out of '+ categories.length);
+    //     });
+    // };
+    let numCompleted = 0;
+    let allLoops = new Promise(async (resolve) => {
+        for(var category in categories) {
+            console.log("Image scraping for: "+categories[category]);
+            ImageScraper.ImageScraper(categories[category]).then(() => {
+                numCompleted++;
+                console.log('\n'+ numCompleted + ' categories completed out of '+ categories.length + '\n');
+                if (numCompleted == categories.length) {
+                    resolve("All loops completed");
+                }
+            });
+            // resolve("One loop completed");
+        };
+        console.log('THIS SHOULD APPEAR IN THE BEGINNING');
+    });
 
+    //Scrape Images one by one category
+    // let numCompleted = 0;
+    // let allLoops = new Promise(async (resolve) => {
+    //     for(var category in categories) {
+    //         console.log("Image scraping for: "+categories[category]);
+    //         await ImageScraper.ImageScraper(categories[category]);
+    //         console.log('\n'+ ++numCompleted + ' categories completed out of '+ categories.length + '\n');
+    //         // resolve("One loop completed");
+    //     };
+    //     console.log('THIS SHOULD ONLY APPEAR AFTER ENTIRE LOOP IS DONE');
+    //     resolve("All loops completed");
+    // });
+
+    let allLoopsDone = await allLoops;
+    console.log(allLoopsDone);
+    
     //We now have images for categories
     //Initialise training process
-    
+    console.log('THIS SHOULD ONLY APPEAR AFTER ALL IMAGES ARE SCRAPED');
 }
 
 module.exports.trainModule = trainModule;
