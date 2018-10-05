181eb34e4c4a63b56d61b55f78e130bd2a9ae7a3
Mark Coetzer <u14044537@tuks.co.za>
2018-10-05 16:02:24 +0200

    Restructured Backend Folders

--- Application Source/Backend/Node Server/src/modules/predictModule.ts
+++ Application Source/Backend/Node Server/src/modules/predictModule.ts
@@ -1,17 +1,15 @@
-/* 
-    File Name: predictModule.js
+/*
+    File Name: predictModule.ts
     Version Number: 1.0.0
     Author Name: Jonathan Lew
     Project Name: Ninshiki
     Organization: Software Sharks
     Update History:
     Functional Description: This module is here to
-        allow for predicting from a model based on 
+        allow for predicting from a model based on
         user input.
 */
 
-var predictModule = function predictModule() {
+export function predictModule() {
     console.log("Predict Module");
 }
-
-module.exports.predictModule = predictModule;
--- Application Source/Backend/Node Server/src/server.ts
+++ Application Source/Backend/Node Server/src/server.ts
@@ -1,4 +1,4 @@
-/* 
+/*
     File Name: server.js
     Version Number: 1.0.0
     Author Name: Jonathan Lew
@@ -8,11 +8,11 @@
     Functional Description: This class is here to
         create a backend server for the dashboard
 */
-const http = require('http');
-const trainModule = require('./modules/trainModule');
-const predictModule = require('./modules/predictModule');
+const http = require("http");
+const trainModule = require("./modules/trainModule");
+const predictModule = require("./modules/predictModule");
 
-const fb = require('./modules/firebaseAdmin');
+const fb = require("./modules/firebaseAdmin");
 fb.initFirebaseAdmin();
 
 const port = process.env.npm_package_config_port;
@@ -21,29 +21,29 @@ let body = "";
 
 const requestHandler = (request, response) => {
     const headers = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-        'Access-Control-Max-Age': 2592000, // 30 days,
-        'Access-Control-Allow-Credentials': 'true'
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Access-Control-Max-Age": 2592000, // 30 days,
+        "Access-Control-Allow-Credentials": "true",
         /** add other headers as per requirement */
     };
 
     // if request = OPTIONS
-    // simply writes the headers to the response and 
+    // simply writes the headers to the response and
     // results in a 204 (No Content) to the browser.
-    if (request.method === 'OPTIONS') {
+    if (request.method === "OPTIONS") {
         response.writeHead(204, headers);
         response.end();
         return;
     }
 
-    if (['GET', 'POST'].indexOf(request.method) > -1) {
+    if (["GET", "POST"].indexOf(request.method) > -1) {
         // 200 - Okay
         response.writeHead(200, headers);
 
         // Todo: Write a quick response here - As confirmation:
-        response.end('Request Accepted');
+        response.end("Request Accepted");
 
         // NB: No need for this to run in the entire request body
         // Create a new request and post it to client once its complete
@@ -51,53 +51,51 @@ const requestHandler = (request, response) => {
 
         if (request.url === "/trainModel") {
 
-            //Todo: Need to find out how dashboard 
-            //will post categories to create
-            let data: string[] = [];
+            // Todo: Need to find out how dashboard
+            // will post categories to create
+            const data = [];
 
             console.log("Training Model");
 
-            request.on('data', chunk => {
+            request.on("data", (chunk) => {
                 data.push(chunk);
             });
-            request.on('end', () => {
+            request.on("end", () => {
                 try {
-                    let categories = JSON.parse(data);
-                    //console.log(categories);
+                    // let categories = JSON.parse(data);
+                    // console.log(categories);
 
-                    //Deal with categories using trainModule.js
+                    // Deal with categories using trainModule.js
 
                     // Todo: Establish after bug testing
                     // trainModule.trainModule(categories);
 
-                    body += "<p>Training Model Now</p>"
-                    body += "<p>" + JSON.stringify(categories) + "</p>";
+                    // body += "<p>Training Model Now</p>"
+                    // body += "<p>" + JSON.stringify(categories) + "</p>";
 
-                    response.writeHead(200, { 'Content-Type': 'text/html' });
+                    response.writeHead(200, { "Content-Type": "text/html" });
                     response.end(body);
                 } catch (err) {
                     if (err instanceof SyntaxError) {
-                        console.error('Syntax Error: ' + (err));
+                        console.error("Syntax Error: " + (err));
                     } else {
-                        console.error('Unknown Error: ' + err);
+                        console.error("Unknown Error: " + err);
                     }
                 }
             });
-        }
-        else if (request.url === "/predict") {
-            //Todo: Need to find out how the 
-            //dashboard will post images to predict
+        } else if (request.url === "/predict") {
+            // Todo: Need to find out how the
+            // dashboard will post images to predict
 
             console.log("Predicting");
             body += "<p>Predicting Now</p>";
 
-            //Deal with image posted using predictModule.js
-            //predictModule.predictModule(img.jpg);
+            // Deal with image posted using predictModule.js
+            // predictModule.predictModule(img.jpg);
 
-            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.writeHead(200, { "Content-Type": "text/html" });
             response.end(body);
-        }
-        else {
+        } else {
             console.log(request.url + " does not exist!");
         }
         return;
@@ -112,8 +110,8 @@ const server = http.createServer(requestHandler);
 
 server.listen(port || 8080, (err) => {
     if (err) {
-        return console.log('ERROR', err);
+        return console.log("ERROR", err);
     }
 
-    console.log('Server is listening on %s', port);
+    console.log("Server is listening on %s", port);
 });
