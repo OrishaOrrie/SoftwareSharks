c2c662a3f59d26cd03d21c3cf820ec96efc711c2
Mark Coetzer <u14044537@tuks.co.za>
2018-10-06 12:11:26 +0200

    Added a test system in Deprecated/

--- Application Source/Backend/Node Server/external_modules/Image Scraper/src/deprecated/test.js
+++ Application Source/Backend/Node Server/external_modules/Image Scraper/src/deprecated/test.js
@@ -1,40 +1,41 @@
-import fs = require('fs');
-import http = require('http');
-import https = require('https');
-import puppeteer = require('puppeteer');
-
-export function Test(searchTerm) {
-    let localPage: puppeteer.Page;
-    let localBrowser: puppeteer.Browser;
-    const urls = [];
-    const dirTerm = searchTerm.replace(/ /g, '_');
-    const dir = 'downloaded_images/' + dirTerm + '/';
+"use strict";
+exports.__esModule = true;
+var fs = require("fs");
+var http = require("http");
+var https = require("https");
+var puppeteer = require("puppeteer");
+function Test(searchTerm) {
+    var localPage;
+    var localBrowser;
+    var urls = [];
+    var dirTerm = searchTerm.replace(/ /g, '_');
+    var dir = 'downloaded_images/' + dirTerm + '/';
     return puppeteer.launch({
-        headless: true,
-    }).then((browser) => {
+        headless: true
+    }).then(function (browser) {
         localBrowser = browser;
         return localBrowser.newPage();
-    }).then((page) => {
+    }).then(function (page) {
         localPage = page;
         return page.goto('https://images.google.com');
-    }).then(() => {
+    }).then(function () {
         // Search term
         return localPage.type('#lst-ib', searchTerm);
-    }).then(() => {
+    }).then(function () {
         // Hit <Enter>
         return localPage.keyboard.press('Enter');
-    }).then(() => {
+    }).then(function () {
         return localPage.waitForSelector('#hdtb-msb-vis');
-    }).then(() => {
+    }).then(function () {
         return localPage.addScriptTag({ url: 'https://code.jquery.com/jquery-3.3.1.min.js' });
-    }).then(() => {
+    }).then(function () {
         // Scroll to the bottom of the page
         console.log('Now I am scrolling to the bottom of the page... this may take a while');
-        return localPage.evaluate(() => {
-            let totalHeight = 0;
-            const distance = 100;
-            const timer = setInterval(() => {
-                const scrollHeight = document.body.scrollHeight;
+        return localPage.evaluate(function () {
+            var totalHeight = 0;
+            var distance = 100;
+            var timer = setInterval(function () {
+                var scrollHeight = document.body.scrollHeight;
                 window.scrollBy(0, distance);
                 totalHeight += distance;
                 if (totalHeight >= scrollHeight) {
@@ -42,66 +43,69 @@ export function Test(searchTerm) {
                 }
             }, 100);
         });
-    }).then(() => {
+    }).then(function () {
         console.log('Reached end of page');
-        return localPage.evaluate(() => {
+        return localPage.evaluate(function () {
             return document.querySelectorAll('.rg_di').length;
         });
-    }).then((numImages) => {
+    }).then(function (numImages) {
         console.log(numImages + ' images found');
         // Get the image source urls of all the images found and add them to an array
-        for (let i = 0; i < numImages; ++i) {
-            localPage.evaluate((j) => {
-                const index = '[data-ri=\"' + j + '\"]';
-                const metaText = (document.querySelector(index).childNodes[2] as HTMLElement).innerHTML;
+        for (var i = 0; i < numImages; ++i) {
+            localPage.evaluate(function (j) {
+                var index = '[data-ri=\"' + j + '\"]';
+                var metaText = document.querySelector(index).childNodes[2].innerHTML;
                 return JSON.parse(metaText).ou;
-            }, i).then((result) => {
+            }, i).then(function (result) {
                 urls.push(result);
             });
         }
-    // }).then(() => {
-    //     return localBrowser.close();
-    }).then(() => {
-        return setTimeout(() => {
+        // }).then(() => {
+        //     return localBrowser.close();
+    }).then(function () {
+        return setTimeout(function () {
             console.log('getting image download URLs');
-            let numDown = 0;
-            // tslint:disable-next-line:prefer-for-of
-            for (let i = 0; i < urls.length; ++i) {
+            var numDown = 0;
+            var _loop_1 = function (i) {
                 if (!fs.existsSync(dir)) {
                     fs.mkdirSync(dir);
                 }
-
-                const fileName = dir + i + '.jpg';
-                const file = fs.createWriteStream(fileName);
-                let protocol;
+                var fileName = dir + i + '.jpg';
+                var file = fs.createWriteStream(fileName);
+                var protocol = void 0;
                 if (urls[i].slice(0, 5) === 'https') {
                     protocol = https;
-                } else {
+                }
+                else {
                     protocol = http;
                 }
-
-                protocol.get(urls[i], (response) => {
+                protocol.get(urls[i], function (response) {
                     response.pipe(file);
-                    file.on('finish', (cb) => {
+                    file.on('finish', function (cb) {
                         file.close();
                         console.log(i + ': ' + urls[i]);
                         numDown++;
                         console.log(numDown + ' images downloaded');
                     });
-                }).on('error', (err, cb) => {
+                }).on('error', function (err, cb) {
                     fs.unlinkSync(fileName);
                     numDown++;
                     console.log(i + ' was deleted');
                     console.log(numDown + ' images completed');
                 });
+            };
+            // tslint:disable-next-line:prefer-for-of
+            for (var i = 0; i < urls.length; ++i) {
+                _loop_1(i);
             }
         }, 30000);
-    }).then(() => {
+    }).then(function () {
         return localBrowser.close();
-    }).then((result) => {
+    }).then(function (result) {
         console.log(result);
         console.log('Finished Scraping for ' + dirTerm);
-    }).catch((error) => {
+    })["catch"](function (error) {
         console.log('Something Broke: ' + error);
     });
 }
+exports.Test = Test;
