734eed200fc092dda49e27dfd6c9a826c736a302
Mark Coetzer <u14044537@tuks.co.za>
2018-10-08 08:22:31 +0200

    Fixed build errors
    Removed duplicate image scraper module

--- Application Source/Backend/Node Server/src/modules/ImageScraper.ts
+++ Application Source/Backend/Node Server/src/modules/ImageScraper.ts
@@ -1,143 +1,143 @@
-import fs = require('fs');
-import http = require('http');
-import https = require('https');
-import puppeteer = require('puppeteer');
-// const spawn = require("child_process").spawn;
+// import fs = require('fs');
+// import http = require('http');
+// import https = require('https');
+// import puppeteer = require('puppeteer');
+// // const spawn = require("child_process").spawn;
 
-let headlessMode;
+// let headlessMode;
 
-if (process.argv[3]) {
-  headlessMode = process.argv[3];
-} else {
-  headlessMode = true;
-}
+// if (process.argv[3]) {
+//   headlessMode = process.argv[3];
+// } else {
+//   headlessMode = true;
+// }
 
-export async function ImageScraper(searchTerm) {
-  return new Promise((resolve, reject) => {
-    (async () => {
-      const browser = await puppeteer.launch({
-        headless: headlessMode,
-      });
+// export async function ImageScraper(searchTerm) {
+//   return new Promise((resolve, reject) => {
+//     (async () => {
+//       const browser = await puppeteer.launch({
+//         headless: headlessMode,
+//       });
 
-      // const searchTerm = process.argv[2];
-      const dirTerm = searchTerm.replace(/ /g, '_');
+//       // const searchTerm = process.argv[2];
+//       const dirTerm = searchTerm.replace(/ /g, '_');
 
-      // Go to Google Images
-      console.log("Don't mind me just popping in to Google Images");
-      const page = await browser.newPage();
-      await page.goto('https://images.google.com');
+//       // Go to Google Images
+//       console.log("Don't mind me just popping in to Google Images");
+//       const page = await browser.newPage();
+//       await page.goto('https://images.google.com');
 
-      // Input the search term and hit Enter
-      console.log('Hello can I get uhhhh some ' + searchTerm);
-      await page.type('#lst-ib', searchTerm);
-      await page.keyboard.press('Enter');
-      await page.waitForSelector('#hdtb-msb-vis');
+//       // Input the search term and hit Enter
+//       console.log('Hello can I get uhhhh some ' + searchTerm);
+//       await page.type('#lst-ib', searchTerm);
+//       await page.keyboard.press('Enter');
+//       await page.waitForSelector('#hdtb-msb-vis');
 
-      await page.addScriptTag({url: 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js'});
+//       await page.addScriptTag({url: 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js'});
 
-      // Scroll to the bottom of the page
-      console.log('Now I am scrolling to the bottom of the page... this may take a while');
-      await page.evaluate(() => {
-        // tslint:disable-next-line:no-shadowed-variable
-        new Promise((resolve, reject) => {
-          let totalHeight = 0;
-          const distance = 100;
-          const timer = setInterval(() => {
-            const scrollHeight = document.body.scrollHeight;
-            window.scrollBy(0, distance);
-            totalHeight += distance;
-            if (totalHeight >= scrollHeight) {
-              clearInterval(timer);
-              resolve();
-            }
-          }, 100);
-        });
-      }).then(() => {
-        console.log('Reached end of page');
-      });
+//       // Scroll to the bottom of the page
+//       console.log('Now I am scrolling to the bottom of the page... this may take a while');
+//       await page.evaluate(() => {
+//         // tslint:disable-next-line:no-shadowed-variable
+//         new Promise((resolve, reject) => {
+//           let totalHeight = 0;
+//           const distance = 100;
+//           const timer = setInterval(() => {
+//             const scrollHeight = document.body.scrollHeight;
+//             window.scrollBy(0, distance);
+//             totalHeight += distance;
+//             if (totalHeight >= scrollHeight) {
+//               clearInterval(timer);
+//               resolve();
+//             }
+//           }, 100);
+//         });
+//       }).then(() => {
+//         console.log('Reached end of page');
+//       });
 
-      let numImages = 0;
-      numImages = await page.evaluate(() => {
-        return document.querySelectorAll('.rg_di').length;
-      });
+//       let numImages = 0;
+//       numImages = await page.evaluate(() => {
+//         return document.querySelectorAll('.rg_di').length;
+//       });
 
-      console.log(numImages + ' images found');
+//       console.log(numImages + ' images found');
 
-      // Get the image source urls of all the images found and add them to an array
-      const urls = [];
-      for (let i = 0; i < numImages; i++) {
-        // tslint:disable-next-line:no-shadowed-variable
-        urls.push( await page.evaluate((i) => {
-          const index = '[data-ri=\"' + i + '\"]';
-          const metaText = (document.querySelector(index).childNodes[2] as HTMLElement).innerHTML;
-          return JSON.parse(metaText).ou;
-        }, i));
-      }
+//       // Get the image source urls of all the images found and add them to an array
+//       const urls = [];
+//       for (let i = 0; i < numImages; i++) {
+//         // tslint:disable-next-line:no-shadowed-variable
+//         urls.push( await page.evaluate((i) => {
+//           const index = '[data-ri=\"' + i + '\"]';
+//           const metaText = (document.querySelector(index).childNodes[2] as HTMLElement).innerHTML;
+//           return JSON.parse(metaText).ou;
+//         }, i));
+//       }
 
-      // get all the images at the urls
-      console.log('getting image download URLs');
-      let numDown = 0;
+//       // get all the images at the urls
+//       console.log('getting image download URLs');
+//       let numDown = 0;
 
-      // tslint:disable-next-line:no-shadowed-variable
-      const looper = new Promise((resolve) => {
-        setTimeout(() => {
-          for (let i = 0; i < urls.length; i++) {
-            // console.log(urls[i]);
-            const j = 0;
+//       // tslint:disable-next-line:no-shadowed-variable
+//       const looper = new Promise((resolve) => {
+//         setTimeout(() => {
+//           for (let i = 0; i < urls.length; i++) {
+//             // console.log(urls[i]);
+//             const j = 0;
 
-            const dir = 'downloaded_images/' + dirTerm + '/';
-            if (!fs.existsSync(dir)) {
-              console.log(dir);
-              fs.mkdirSync(dir);
-            }
+//             const dir = 'downloaded_images/' + dirTerm + '/';
+//             if (!fs.existsSync(dir)) {
+//               console.log(dir);
+//               fs.mkdirSync(dir);
+//             }
 
-            const fileName = dir + i + '.jpg';
-            const file = fs.createWriteStream(fileName);
-            let protocol; // = http | https;
-            if (urls[i].slice(0, 5) === 'https') {
-              protocol = https;
-            } else {
-              protocol = http;
-            }
+//             const fileName = dir + i + '.jpg';
+//             const file = fs.createWriteStream(fileName);
+//             let protocol; // = http | https;
+//             if (urls[i].slice(0, 5) === 'https') {
+//               protocol = https;
+//             } else {
+//               protocol = http;
+//             }
 
-            const request = protocol.get(urls[i], (response) => {
-              response.pipe(file);
-              file.on('finish', (cb) => {
-                file.close();
-                console.log(i + ': ' + urls[i]);
-                numDown++;
-                console.log(numDown + ' images downloaded');
+//             const request = protocol.get(urls[i], (response) => {
+//               response.pipe(file);
+//               file.on('finish', (cb) => {
+//                 file.close();
+//                 console.log(i + ': ' + urls[i]);
+//                 numDown++;
+//                 console.log(numDown + ' images downloaded');
 
-                /**
-                 * If the number of images processed is within 50 of the total images identified,
-                 * then resolve this promise
-                 */
-                if (numDown > numImages - 50) {
-                  resolve('\nYEEEEEEEET\n');
-                  clearTimeout(); // doesn't work
-                  return; // doesn't work
-                }
-                // if (cb) { cb(err.message); }
-              });
-            }).on('error', (err, cb) => {
-              fs.unlinkSync(fileName);
-              numDown++;
-              console.log(i + ' was deleted');
-              console.log(numDown + ' images completed');
-              // if (cb) cb(err.message);
-            });
+//                 /**
+//                  * If the number of images processed is within 50 of the total images identified,
+//                  * then resolve this promise
+//                  */
+//                 if (numDown > numImages - 50) {
+//                   resolve('\nYEEEEEEEET\n');
+//                   clearTimeout(); // doesn't work
+//                   return; // doesn't work
+//                 }
+//                 // if (cb) { cb(err.message); }
+//               });
+//             }).on('error', (err, cb) => {
+//               fs.unlinkSync(fileName);
+//               numDown++;
+//               console.log(i + ' was deleted');
+//               console.log(numDown + ' images completed');
+//               // if (cb) cb(err.message);
+//             });
 
-          }
-        }, 30000);
-      });
+//           }
+//         }, 30000);
+//       });
 
-      await browser.close();
+//       await browser.close();
 
-      const result = await looper;
-      console.log(result);
-      resolve();
-      console.log('Finished Scraping for ' + dirTerm);
-    })();
-  });
+//       const result = await looper;
+//       console.log(result);
+//       resolve();
+//       console.log('Finished Scraping for ' + dirTerm);
+//     })();
+//   });
 
-}
+// }
